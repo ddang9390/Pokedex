c3d775b2bd1dc4a1f184f0471e068f31878ffa83
Daniel Dang <dnld1ng@yahoo.com>
2024-05-18 11:53:25 -0400

    Basic REPL

--- main.go
+++ main.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func()
+}
+
+var exit int = 0
+
+func commands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
+func commandHelp() {
+	fmt.Println("Type 'exit' to exit the program")
+}
+
+func commandExit() {
+	fmt.Println("Closing the pokedex")
+	exit = 1
+}
+
+func printPrompt() {
+	fmt.Printf("Pokedex > ")
+}
 
 func main() {
-	fmt.Println("Hello World")
+	commands := commands()
+	reader := bufio.NewScanner(os.Stdin)
+
+	printPrompt()
+	for reader.Scan() {
+		text := strings.ToLower(strings.TrimSpace(reader.Text()))
+		if command, exists := commands[text]; exists {
+			command.callback()
+		} else {
+			fmt.Println("Invalid command")
+		}
+		if exit == 1 {
+			return
+		}
+		printPrompt()
+	}
+	fmt.Println()
 }
